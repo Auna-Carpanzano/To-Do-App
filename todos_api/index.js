61f1c4962a21acd04f9104047fdf117f155e001f
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-26 14:02:31 -0700

    Add var todoRoutes that requires todos file from routes directory

--- todos_api/index.js
+++ todos_api/index.js
@@ -2,6 +2,8 @@ var express = require("express"),
     app = express(),
     port = process.env.PORT || 3000;
 
+var todoRoutes = require("./routes/todos");
+
 app.listen(port, function() {
   console.log("App is running");
 });
