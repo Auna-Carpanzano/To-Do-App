bbd1b68b47c8bb3b08a4c6406950a5ba408df550
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-26 15:49:04 -0700

    Refactor the individual todo, update, and delete routes by accessing .getTodo, .updateTodo, and .deleteTodo through helpers var

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -7,37 +7,9 @@ router.route("/")
   .get(helpers.getTodos)
   .post(helpers.createTodo)
 
-// RETRIEVE INDIVIDUAL TODO
-router.get("/:todoId", function(req, res) {
-  db.Todo.findById(req.params.todoId)
-  .then(function(foundTodo) {
-    res.json(foundTodo);
-  })
-  .catch(function(err) {
-    res.send(err);
-  })
-});
-
-// UPDATE ROUTE
-router.put("/:todoId", function(req, res) {
-  db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-  .then(function(todo) {
-    res.json(todo);
-  })
-  .catch(function(err) {
-    res.send(err);
-  })
-});
-
-// DELETE ROUTE
-router.delete("/:todoId", function(req, res) {
-  db.Todo.remove({_id: req.params.todoId})
-  .then(function() {
-    res.json({message: "To-do deleted!"})
-  })
-  .catch(function(err) {
-    res.send(err)
-  })
-});
+router.route("/:todoId")
+  .get(helpers.getTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo)
 
 module.exports = router;
