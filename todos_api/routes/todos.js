1056b8735700c93f7de338f573fe5c2c7b0e11f9
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-26 14:11:13 -0700

    Use mongoose .find() method to find all Todos in database

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -2,4 +2,8 @@ var express = require("express"),
     router = express.Router(),
     db = require("../models");
 
+router.get("/", function(req, res) {
+  db.Todo.find()
+});
+
 module.exports = router;
