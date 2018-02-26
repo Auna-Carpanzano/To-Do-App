212902f6d68e5753b3677179622fa810bba3cbe6
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-26 15:21:34 -0700

    Add .then to send json message as response

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -49,6 +49,9 @@ router.put("/:todoId", function(req, res) {
 // DELETE ROUTE
 router.delete("/:todoId", function(req, res) {
   db.Todo.remove({_id: req.params.todoId})
+  .then(function() {
+    res.json({message: "To-do deleted!"})
+  })
 });
 
 module.exports = router;
